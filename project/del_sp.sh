f3246f690b377ae7080801cdf222cde97ea402ef
wangkaichao <wangkaichao@skyworth.com>
2019-01-22 15:15:45 +0800

    提交一个脚本，每次改完代码运行该脚本，可以删除行尾空格，tab
    Change-Id: Ie91393082f4f604ec9f1a4953716a8d42406b3dd

---
+++ project/del_sp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#git status | grep -E '*\.mk$|*\.c$|*\.h$|*\.cpp$|*\.hpp$' | cut -f2 -d: | xargs sed -i 's/[ ]*$//g'
+git status | grep -E '*\.mk$|*\.c$|*\.h$|*\.cpp$|*\.hpp$' | cut -f2 -d: | sed 's/^[ \t]*//g' | xargs sed -i 's/[ ]*$//g'
