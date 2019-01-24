9ebd4e571cbb125934d04e8aac71135148544179
wangkaichao <wangkaichao@skyworth.com>
2019-01-24 14:47:24 +0800

    测试
    Change-Id: I2b9e85037d14241ea9e893cac6ffc6b877286be3

--- project/del_sp.sh
+++ project/del_sp.sh
@@ -2,3 +2,4 @@
 
 #git status | grep -E '*\.mk$|*\.c$|*\.h$|*\.cpp$|*\.hpp$' | cut -f2 -d: | xargs sed -i 's/[ ]*$//g'
 git status | grep -E '*\.mk$|*\.c$|*\.h$|*\.cpp$|*\.hpp$' | cut -f2 -d: | sed 's/^[ \t]*//g' | xargs sed -i 's/[ ]*$//g'
+
