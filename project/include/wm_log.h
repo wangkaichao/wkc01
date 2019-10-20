43f6e88cddf441931d433441e409172b67a9c7a4
wangkaichao <364596530@qq.com>
2019-10-20 18:56:31 +0800

    add wm include

---
+++ project/include/wm_log.h
@@ -0,0 +1,83 @@
+#ifndef WM_LOG_H
+#define WM_LOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LOGD    printf
+#define LOGI    printf
+#define LOGW    printf
+#define LOGE    printf
+
+#define CHK_ARG(val)                            \
+do {                                            \
+    if ((val)) {                                \
+        LOGE("%s %d: Invalid Parameter", __func__, __LINE__); \
+    }                                           \
+} while (0)
+
+#define CHK_ARG_RV(val)                         \
+do {                                            \
+    if ((val)) {                                \
+        LOGE("%s %d: Invalid Parameter", __func__, __LINE__); \
+        return;                                 \
+    }                                           \
+} while (0)
+
+#define CHK_ARG_RE(val, err)                    \
+do {                                            \
+    if ((val)) {                                \
+        LOGE("%s %d: Invalid Parameter", __func__, __LINE__); \
+        return err;                             \
+    }                                           \
+} while (0)
+
+#define CHK_FUN(func)                           \
+do {                                            \
+    int ret = func;                             \
+    if (ret != 0) {                             \
+        LOGE("%s %d: %s failed %d(%#x)", __func__, __LINE__, #func, ret, ret); \
+    }                                           \
+} while (0)
+
+#define CHK_FUN_RV(func)                        \
+do {                                            \
+    int ret = func;                             \
+    if (ret != 0) {                             \
+        LOGE("%s %d: %s failed %d(%#x)", __func__, __LINE__, #func, ret, ret); \
+        return;                                 \
+    }                                           \
+} while (0)
+
+#define CHK_FUN_RE(func, err)                   \
+do {                                            \
+    int ret = func;                             \
+    if (ret != 0) {                             \
+        LOGE("%s %d: %s failed %d(%#x)", __func__, __LINE__, #func, ret, ret); \
+        return err;                             \
+    }                                           \
+} while (0)
+
+#define CHK_FUN_GT(func, here)                  \
+do {                                            \
+    int ret = func;                             \
+    if (ret != 0) {                             \
+        LOGE("%s %d: %s failed %d(%#x)", __func__, __LINE__, #func, ret, ret); \
+        goto here;                              \
+    }                                           \
+} while (0)
+
+#define CHK_FUN_EQ(func, ret, err)              \
+do {                                            \
+    ret = func;                                 \
+    if (ret != err) {                           \
+        LOGE("%s %d: %s failed", __func__, __LINE__, #func); \
+    }                                           \
+} while (0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ project/include/wm_type.h
@@ -0,0 +1,16 @@
+#ifndef WM_TYPE_H
+#define WM_TYPE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX(x, y)   (x) > (y) ? (x) : (y)
+#define MIN(x, y)   (x) < (y) ? (x) : (y)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
